7ee74f902c7f4d17cf1883728d6af9994b5f3fe6
Jangyusu <dbtnss@naver.com>
2020-05-02 21:51:10 +0900

    util js 주석

--- js/util.js
+++ js/util.js
@@ -1,19 +1,19 @@
-function addActive(target) {
+function addActive(target) { //active Class 추가
     target.addClass("active");
 }
 
-function removeActive(target) {
+function removeActive(target) { //active Class 제거
     target.removeClass("active");
 }
 
-function toggleActive(target) {
+function toggleActive(target) { //active Class 추가 및 제거
     target.toggleClass("active");
 }
 
-function addHide(target) {
+function addHide(target) { //hide Class 추가
     target.addClass("hide");
 }
 
-function removeHide(target) {
+function removeHide(target) { //hide Class 제거
     target.removeClass("hide");
 }
